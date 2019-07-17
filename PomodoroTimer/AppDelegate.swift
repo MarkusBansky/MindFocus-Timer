b525fe9f5c0e9313fa92aea43286a0fc9433c866
Markiian Benovskyi <benovskym@gmail.com>
2019-07-17 10:03:30 +0100

    Initial Commit

---
+++ PomodoroTimer/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  PomodoroTimer
+//
+//  Created by Markiian Benovskyi on 17/07/2019.
+//  Copyright © 2019 Markiian Benovskyi. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
